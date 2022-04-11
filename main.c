9184890a99b44a4349a599f9afcd0db309b387b8
David <github@davidepalma.it>
2022-04-11 23:26:43 +0200

    Fatto

--- main.c
+++ main.c
@@ -4,16 +4,40 @@
  * Embedded Control course at POLIBA.
  */
 
+#include "lib-periph.h"
 #include "mcu-api.h"
 #include <stdio.h>
-#include <stdint.h>
 
-/* Recall FOO_PERIPH is the address to the memory mapped registers of the peripheral */
+/* Recall FOO_PERIPH is the address to the memory mapped FooPeriph_t of the peripheral */
 /* Of course you can implement functions to be called in the main if you need */
 
-void main()
+
+#define NUMBERS 100
+int main()
 {
-    printf("Hello Tartarus MCU world!\n");
-    MCU_STEP; /* This simulates that you have performed on tick. It's just an example */
+    /*Read 10 32-bit unsigned int readings from the
+peripheral
+2. Compute the average value of the 10 received
+readings
+3. Send the average value (32-bit unsigned int) to the
+peripheral
+4. Go back to step 1
+*/
+    printf("Hello, going to init device\n");
+    foo_periph_init(FOO_PERIPH, 32, 33);
+    printf("Initialized device\n");
+    uint32_t values[NUMBERS];
+    for (int i =0 ; i< NUMBERS; i++){
+        values[i] = foo_periph_rx_data(FOO_PERIPH);
+        printf("Read value %d\n", values[i]);
+    }
+    uint64_t average = 0;
+    for (int i =0 ; i< NUMBERS; i++){
+        average += values[i];
+    }
+    average = average/NUMBERS;
+    uint32_t lsb = average & 0xffffffff;
+    printf("Average value is %d\n", lsb);
+    foo_periph_tx_data(FOO_PERIPH, lsb);
 }
 
