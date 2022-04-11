6f872ef26db27dd8bff635afe257da2278fe8dec
David <github@davidepalma.it>
2022-04-11 22:10:39 +0200

    moved files, .gitignore, cmakelist

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+/* Author: _____PUT YOUR GROUP NAME HERE____
+ *
+ * This source is part of the Tatarus MCU assignment for the
+ * Embedded Control course at POLIBA.
+ */
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include "mcu-api.h"
+#include <stdio.h>
+#include <stdint.h>
+
+/* Recall FOO_PERIPH is the address to the memory mapped registers of the peripheral */
+/* Of course you can implement functions to be called in the main if you need */
+
+void main()
+{
+    printf("Hello Tartarus MCU world!\n");
+    MCU_STEP; /* This simulates that you have performed on tick. It's just an example */
 }
+
